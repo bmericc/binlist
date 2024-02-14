8b0657b2a0673f1723b29cb3152665e15f0eaf81
Bahri Meriç Canlı <bahri@bahri.info>
2024-02-14 10:15:01 +0300

    iş bankası bin list

--- tools/yenibin-converter.php
+++ tools/yenibin-converter.php
@@ -1,19 +1,19 @@
 <?php
 
-$json = file_get_contents("csvjson.json");
+$json = file_get_contents("csv8json.json");
 $array = json_decode($json, true);
 $keys = array_keys(current($array) );
 
 $binArray = array();
 foreach($array as $row) {
 	foreach($keys as $key) {
-		$bin_code = trim($row["bin_code"]);
+		$bin_code = trim($row["bin8_code"]);
 		$binArray[$bin_code][$key] = trim($row[$key]);
 	}
 }
 
 $json = json_encode($binArray);
-file_put_contents("new_binlist.json", $json);
+file_put_contents("new_bin8list.json", $json);
 
 
 
